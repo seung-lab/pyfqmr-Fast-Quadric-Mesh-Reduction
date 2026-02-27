813425745ae259cf5187a0475f5ad660349f3e11
William Silversmith <william.silversmith@gmail.com>
2026-02-26 21:08:03 -0500

    fix: remove duplicate macro definition

--- pyfqmr/Simplify.h
+++ pyfqmr/Simplify.h
@@ -20,7 +20,6 @@
 #include <float.h> //FLT_EPSILON, DBL_EPSILON
 
 #define loopi(start_l,end_l) for ( int i=start_l;i<end_l;++i )
-#define loopi(start_l,end_l) for ( int i=start_l;i<end_l;++i )
 #define loopj(start_l,end_l) for ( int j=start_l;j<end_l;++j )
 #define loopk(start_l,end_l) for ( int k=start_l;k<end_l;++k )
 
